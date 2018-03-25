2937b28aa163b9cbf8916f1a726ff1a16d2a698c
ruge23 <ruizgerezr@gmail.com>
2018-03-25 02:27:24 -0300

    add index.ejs

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,16 @@ router.get('/', (req, res)=>{
     res.render('selectName', { nombre: null});
 })
 
+// saludo
+router.post('/',(req, res)=>{
+    res.redirect(`/${req.body.nombre}`)
+})
+
+router.get('/:nombre', (req, res)=>{
+    res.render('index', { nombre: req.params.nombre })
+})
+
+
 
 
 
