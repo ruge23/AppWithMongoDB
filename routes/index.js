ee8f78a51fc908a53081c8319da15d45e4a65f0e
ruge23 <ruizgerezr@gmail.com>
2018-03-25 21:40:34 -0300

    add Profile

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,32 @@ router.get('/:nombre/pokemon',(req, res)=>{
     })
 })
 
+//agregar un pokemon
+router.get('/:nombre/newPokemon', (req, res)=>{
+    res.render('addPokemon',{nombre: req.params.nombre})
+})
+
+router.post('/:nombre/newPokemon', (req, res)=>{
+    console.log(req.body);
+    Pokemon.create(req.body, (err, poke)=>{
+        if(err) return res.send(err);
+        res.redirect(`${req.params.nombre}/pokemon/${poke.nombre}`)
+    })
+})
+
+// Perfil pokemon
+router.get('/:nombre/pokemon/:pokemon', (req, res)=>{
+    Pokemon.findOne({nombre: req.params.pokemon})
+        .exec((err, poke)=>{
+            if(err) return err;
+            console.log('pokemon', poke)
+            res.render('pokemonProfile',{
+                pokemon: poke,
+                nombre: req.params.nombre,
+            })
+        })
+})
+
 
 
 
